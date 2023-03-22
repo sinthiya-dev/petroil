8b43a4d1311f570025fdd60e450249aed664f274
Umme Habiba Sinthiya <ummehabibas761@gmail.com>
2023-03-22 15:53:47 +0600

    rain

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+
+$('.slide_main').slick({
+    dots: false,
+    infinite: true,
+     autoplay: true,
+    arrows:false,
+    autoplaySpeed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
